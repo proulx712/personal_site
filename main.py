8178c25b246ca721912d3977a54e0a7669419431
EricP <proulx712@gmail.com>
2017-09-24 11:25:13 -0700

    added vue stuff

--- main.py
+++ main.py
@@ -2,34 +2,52 @@
 import logging
 
 from flask import Flask, render_template
-from flask import send_file
 
-app = Flask(__name__)
+
+class CustomFlask(Flask):
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+        block_start_string='(%',
+        block_end_string='%)',
+        variable_start_string='((',
+        variable_end_string='))',
+        comment_start_string='(#',
+        comment_end_string='#)',
+    ))
+
+
+app = CustomFlask(__name__)
 
 
 @app.route('/', methods=['GET'])
-def get_home():
-    return render_template('pages/home.html')
+def get_index():
+    return render_template('index.html')
 
 
-@app.route('/resume', methods=['GET'])
-def get_resume():
-    return render_template('pages/resume.html')
-
-
-@app.route('/download', methods=['POST'])
-def download_resume():
-    return send_file('docs/PROULXRESUME.pdf', as_attachment=True)
-
-
-@app.route('/contact', methods=['GET'])
-def get_contact():
-    return render_template('pages/contact.html')
-
-
-@app.route('/about', methods=['GET'])
-def get_about():
-    return render_template('pages/about.html')
+#
+# @app.route('/', methods=['GET'])
+# def get_home():
+#     return render_template('pages/home.html')
+#
+#
+# @app.route('/resume', methods=['GET'])
+# def get_resume():
+#     return render_template('pages/resume.html')
+#
+#
+# @app.route('/download', methods=['POST'])
+# def download_resume():
+#     return send_file('docs/PROULXRESUME.pdf', as_attachment=True)
+#
+#
+# @app.route('/contact', methods=['GET'])
+# def get_contact():
+#     return render_template('pages/contact.html')
+#
+#
+# @app.route('/about', methods=['GET'])
+# def get_about():
+#     return render_template('pages/about.html')
 
 
 @app.errorhandler(500)
